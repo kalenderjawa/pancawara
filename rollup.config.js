dd21a77c0b101355190bfd124527807fb0de9371
Equan Pr <equan.p@gmail.com>
2021-09-13 09:05:44 +0700

    konfirgurasi rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.js",
+  plugins: [terser()],
+  output: [
+    {
+      file: pkg.module,
+      format: "esm",
+    },
+    {
+      file: pkg.browser,
+      format: "iife",
+      name: "Pancawara",
+    },
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+  ],
+};
